86a6069be05eba0e2bf5456096c3c1533f1d1b35
Robert Bennett <rltbennett@icloud.com>
2020-04-09 10:05:06 -0400

    Added script to download songs from YouTube

---
+++ download_songs.py
@@ -0,0 +1,34 @@
+# %%
+import subprocess
+from urllib.parse import quote_plus
+
+import pandas as pd
+
+
+def fetch_all():
+    df = pd.read_csv("songs_meta_pre.csv")
+    df: pd.DataFrame
+
+    for i, row in enumerate(df.iterrows()):
+
+        row = row[1]
+        search_string = " ".join([row["title"], row["artist"], "song"])
+        search_string = quote_plus(search_string)
+        url = (
+            f"https://www.youtube.com/results?search_query={search_string}&sp=CAMSAhAB"
+        )
+        subprocess.check_call(
+            [
+                "osascript",
+                "-e",
+                'tell application "Keyboard Maestro Engine" '
+                + 'to do script "CD02385B-8868-491E-B54F-D9714BA3459A" '
+                + f'with parameter "{url}"',
+            ]
+        )
+        print(i + 1, f"Got {url}")
+
+    return df
+
+
+fetch_all()
